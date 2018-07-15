6025871b5af1bbb368a55eedb27cd028e064f4c2
as <as.utf8@gmail.com>
2018-07-15 16:43:55 -0700

    add general pkg with default impl

---
+++ driver/internal/swizzle/swizzle.go
@@ -0,0 +1,14 @@
+package swizzle
+
+var (
+	swizzler = pureBGRA
+)
+
+func pureBGRA(p, q []byte) {
+	if len(p)%4 != 0 {
+		return
+	}
+	for i := 0; i < len(p); i += 4 {
+		q[i+0], q[i+1], q[i+2], q[i+3] = p[i+2], p[i+1], p[i+0], p[i+3]
+	}
+}
