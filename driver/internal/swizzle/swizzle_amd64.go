daf6a3813d7bffa8cd606661017ea00ffe6c4516
as <as.utf8@gmail.com>
2018-07-15 08:23:06 -0700

    add non-avx2 swizzle support

--- driver/internal/swizzle/swizzle_amd64.go
+++ driver/internal/swizzle/swizzle_amd64.go
@@ -1,25 +1,44 @@
-// Copyright 2015 The Go Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style
-// license that can be found in the LICENSE file.
-
+// Copyright 2018 as
+// Copyright 2015 The Go Authors
 package swizzle
 
-// haveSSSE3 returns whether the CPU supports SSSE3 instructions (i.e. PSHUFB).
-//
-// Note that this is SSSE3, not SSE3.
+// haveSSSE3 is true on CPUs with SSSE3 (i.e. PSHUFB).
 func haveSSSE3() bool
+
+// haveAVX2 is true on CPUs with AVX2 (i.e. VPSHUFB).
 func haveAVX2() bool
 
-var useBGRA32 = haveAVX2()
-var useBGRA16 = haveSSSE3()
+var (
+	useBGRA32 = haveAVX2()
+	useBGRA16 = haveSSSE3()
+	useBGRA4 = true
+	
+	swizzler func(p, q []byte)
+)
 
-const useBGRA4 = true
+func init() {
+	swizzler = bgra4sd
+	if useBGRA16 {
+		swizzler = bgra16sd
+	}
+	if useBGRA32 {
+		swizzler = bgra256sd
+	}
+}
 
-// go test
+func BGRASD(p, q []byte) {
+	if len(p) < 4 {
+		return
+	}
+	swizzler(p,q)
+}
+
+// AVX2
 func bgra256sd(p, q []byte)
+func bgra128sd(p,q []byte)
 
-func bgra256(p []byte)
-func bgra64(p []byte)
-func bgra32(p []byte)
-func bgra16(p []byte)
-func bgra4(p []byte)
+// SSSE
+func bgra16sd(p,q []byte)
+
+// AMD64
+func bgra4sd(p,q []byte)
