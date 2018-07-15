dae6e6e2d7a69983b3d5f0987bac897347c6d7bf
as <as.utf8@gmail.com>
2018-07-15 16:43:44 -0700

    remove pure swizzler from test

--- driver/internal/swizzle/swizzle_test.go
+++ driver/internal/swizzle/swizzle_test.go
@@ -136,15 +136,6 @@ func TestBGRARandom(t *testing.T) {
 	}
 }
 
-func pureBGRA(p, q []byte) {
-	if len(p)%4 != 0 {
-		return
-	}
-	for i := 0; i < len(p); i += 4 {
-		q[i+0], q[i+1], q[i+2], q[i+3] = p[i+2], p[i+1], p[i+0], p[i+3]
-	}
-}
-
 func BenchmarkSwizzle(b *testing.B) {
 	sizes := []string{"64B", "64K", "64M"}
 	for name, fn := range supported {
