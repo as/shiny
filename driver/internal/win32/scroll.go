53b888539d84f71e845d81c86bdc5a9959d8d313
as <as.utf8@gmail.com>
2018-09-29 02:20:14 -0700

    scroll: seperate into scroll.go

---
+++ driver/internal/win32/scroll.go
@@ -0,0 +1,36 @@
+// Copyright 2015 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// +build windows
+
+type Scroll = mouse.Event
+
+func sendScrollEvent(hwnd syscall.Handle, uMsg uint32, wParam, lParam uintptr) (lResult uintptr) {
+	e := mouse.Event{
+		X:         float32(_GET_X_LPARAM(lParam)),
+		Y:         float32(_GET_Y_LPARAM(lParam)),
+		Modifiers: keyModifiers(),
+	}
+	e.Direction = mouse.DirStep
+	// Convert from screen to window coordinates.
+	p := _POINT{
+		int32(e.X),
+		int32(e.Y),
+	}
+	_ScreenToClient(hwnd, &p)
+	e.X = float32(p.X)
+	e.Y = float32(p.Y)
+	delta := _GET_WHEEL_DELTA_WPARAM(wParam) / _WHEEL_DELTA
+	switch {
+	case delta > 0:
+		e.Button = mouse.ButtonWheelUp
+	case delta < 0:
+		e.Button = mouse.ButtonWheelDown
+		delta = -delta
+	default:
+		return
+	}
+	screen.SendScroll(e)
+	return
+}
--- driver/internal/win32/win32.go
+++ driver/internal/win32/win32.go
@@ -71,7 +71,6 @@ func newWindow(opts *screen.NewWindowOptions) (syscall.Handle, error) {
 		return 0, err
 	}
 	
-	
 	// h := syscall.Handle(0)
 	// if opts.Overlay{
 //		h = GetConsoleWindow()
@@ -175,7 +174,6 @@ func sendSize(hwnd syscall.Handle) {
 }
 
 type Lifecycle = lifecycle.Event
-type Scroll = mouse.Event
 type Mouse = mouse.Event
 type Key = key.Event
 type Size = size.Event
@@ -184,38 +182,6 @@ type Paint = paint.Event
 func sendClose(hwnd syscall.Handle, uMsg uint32, wParam, lParam uintptr) (lResult uintptr) {
 	LifecycleEvent(hwnd, lifecycle.StageDead)
 	return 0
-}
-
-func sendScrollEvent(hwnd syscall.Handle, uMsg uint32, wParam, lParam uintptr) (lResult uintptr) {
-	if uMsg != _WM_MOUSEWHEEL {
-		panic("sendScrollEvent: not a scroll message")
-	}
-	e := mouse.Event{
-		X:         float32(_GET_X_LPARAM(lParam)),
-		Y:         float32(_GET_Y_LPARAM(lParam)),
-		Modifiers: keyModifiers(),
-	}
-	e.Direction = mouse.DirStep
-	// Convert from screen to window coordinates.
-	p := _POINT{
-		int32(e.X),
-		int32(e.Y),
-	}
-	_ScreenToClient(hwnd, &p)
-	e.X = float32(p.X)
-	e.Y = float32(p.Y)
-	delta := _GET_WHEEL_DELTA_WPARAM(wParam) / _WHEEL_DELTA
-	switch {
-	case delta > 0:
-		e.Button = mouse.ButtonWheelUp
-	case delta < 0:
-		e.Button = mouse.ButtonWheelDown
-		delta = -delta
-	default:
-		return
-	}
-	screen.SendScroll(e)
-	return
 }
 
 // Precondition: this is called in immediate response to the message that triggered the event (so not after w.Send).
