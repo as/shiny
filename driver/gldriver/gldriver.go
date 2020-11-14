a307ce21a4f484c58adfcb14b548bc42ac2dad0e
as <as.utf8@gmail.com>
2020-11-14 14:55:16 -0800

    gldriver: darwin only

--- driver/gldriver/gldriver.go
+++ driver/gldriver/gldriver.go
@@ -2,6 +2,8 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
+// +build darwin
+
 // Package gldriver provides an OpenGL driver for accessing a screen.
 package gldriver // import "github.com/as/shiny/driver/gldriver"
 
